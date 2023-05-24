977e63769a38e84ac0b0ce610f884aae8fd006c6
Mateusz Gierczak <mateusz.gierczak33@gmail.com>
2023-05-24 20:27:10 +0200

    Implement insertion sort algorithm

--- sorting/insertion-sort/include/insertion_sort.hpp
+++ sorting/insertion-sort/include/insertion_sort.hpp
@@ -1,1 +1,18 @@
 #pragma once
+
+template<typename Range>
+void insertionSort(Range& range)
+{
+    for(int j = 1; j < std::size(range); ++j)
+    {
+        auto key = range[j];
+        auto i = j - 1;
+
+        while(i >= 0 and range[i] > key)
+        {
+            range[i + 1] = range[i];
+            i--;
+        }
+        range[i + 1] = key;
+    }
+}
--- sorting/insertion-sort/ut/InsertionSortTests.cpp
+++ sorting/insertion-sort/ut/InsertionSortTests.cpp
@@ -1,2 +1,12 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include "insertion_sort.hpp"
 
+using namespace ::testing;
+
+TEST(InsertionSortTest, shouldSortInAscendingOrder)
+{
+    std::vector<int> vec {5, 2, 4, 6, 1, 3};
+    insertionSort(vec);
+    EXPECT_THAT(vec, ElementsAre(1, 2, 3, 4, 5, 6));
+}
